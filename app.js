4e270a9ca4342afe8c8f330f3465b22b9d293590
ShubhaKrushna <pawarshrutika197@gmail.com>
2023-12-11 17:48:56 +0530

    port number

--- app.js
+++ app.js
@@ -20,6 +20,37 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
 app.use(methodOverride('_method'));
+const store = MongoStore.create({
+  mongoUrl: process.env.MONGODB_URI
+})
+const { MongoClient } = require('mongodb');
+
+// MongoDB connection URL
+const mongoUrl = 'mongodb://localhost:27017/mydatabase';
+
+// Create a MongoDB client
+const clientPromise = MongoClient.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
+
+// Example using the client promise
+clientPromise
+  .then((client) => {
+    // Do something with the MongoDB client, such as performing database operations
+    const db = client.db('mydatabase');
+    const collection = db.collection('mycollection');
+
+    // Example: Insert a document
+    return collection.insertOne({ key: 'value' });
+  })
+  .then((result) => {
+    console.log('Document inserted:', result);
+  })
+  .catch((error) => {
+    console.error('Error:', error);
+  })
+  .finally(() => {
+    // Close the MongoDB client when done
+    clientPromise.then((client) => client.close());
+  });
 
 app.use(session({
   secret: 'keyboard cat',
@@ -49,3 +80,5 @@ app.listen(PORT, ()=> {
   console.log(`App listening on port ${PORT}`);
 });
 
+// app.use('/.netlify/app',router);
+// module.exports.handler=serverless(app);
